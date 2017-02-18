a0efd0e7061246af9cee22334c88559b2ade9514
Evan Forbes <forbes.evan@gmail.com>
2017-02-18 13:53:30 -0500

    Elevator Changes

--- src/org/usfirst/frc/team3507/robot/commands/IntakeElevator.java
+++ src/org/usfirst/frc/team3507/robot/commands/IntakeElevator.java
@@ -1,15 +1,23 @@
 package org.usfirst.frc.team3507.robot.commands;
 
 import org.usfirst.frc.team3507.robot.Robot;
+import org.usfirst.frc.team3507.robot.subsystems.Preferences;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 public class IntakeElevator extends Command {
 
-	
+	Preferences prefs = Preferences.getInstance(); 
 	public IntakeElevator(double speed) {
 		requires(Robot.elevator);
 		
+	}
+	protected void execute() {
+		Robot.elevator.ElevatorIntake.setP(prefs.getDouble("ElevatorP", 0));
+		Robot.elevator.ElevatorIntake.setI(prefs.getDouble("ElevatorI", 0));
+		Robot.elevator.ElevatorIntake.setD(prefs.getDouble("ElevatorD", 0));
+		Robot.elevator.ElevatorIntake.setF(prefs.getDouble("ElevatorF", 0));
+		Robot.flywheel.go(prefs.getDouble("Elevator Speed", .5));
 	}
 	@Override
 	protected boolean isFinished() {
--- src/org/usfirst/frc/team3507/robot/subsystems/ElevatorIntake.java
+++ src/org/usfirst/frc/team3507/robot/subsystems/ElevatorIntake.java
@@ -3,12 +3,26 @@ package org.usfirst.frc.team3507.robot.subsystems;
 import org.usfirst.frc.team3507.robot.Robot;
 import org.usfirst.frc.team3507.robot.RobotMap;
 
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
+
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class ElevatorIntake extends Subsystem {
 
 	public com.ctre.CANTalon ElevatorIntake = new com.ctre.CANTalon(RobotMap.elevatorMotor);
 	
+	public CANTalon elevator = new CANTalon(RobotMap.elevatorMotor);
+	
+	public ElevatorIntake() {
+		elevator.enableBrakeMode(true);
+		elevator.setFeedbackDevice(FeedbackDevice.CtreMagEncoder_Relative);
+		elevator.reverseSensor(true);
+		elevator.changeControlMode(TalonControlMode.Speed);
+		elevator.set(0);
+	}
+
 	@Override
 	protected void initDefaultCommand() {
 		// TODO Auto-generated method stub
@@ -16,18 +30,14 @@ public class ElevatorIntake extends Subsystem {
 	}
 	double speed;
 	
-	public ElevatorIntake() {
-		//this.speed = speed;
-	}
-	
 	public void go(double speed)
     {
     	if (speed > 0) {
-    		ElevatorIntake.set(-.7);
+    		//ElevatorIntake.set(-.7);
     	}
     	else stop();
     }
-    
+	
     public void stop()
     {
     	ElevatorIntake.set(0);
