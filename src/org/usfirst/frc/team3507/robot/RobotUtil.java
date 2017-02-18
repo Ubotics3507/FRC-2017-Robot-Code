a3d53abc9b19f7241b5ab92473e1973d73dfebfb
Christian <christianjlin@gmail.com>
2017-02-17 19:39:28 -0600

    Square Deadzone

--- src/org/usfirst/frc/team3507/robot/RobotUtil.java
+++ src/org/usfirst/frc/team3507/robot/RobotUtil.java
@@ -3,8 +3,14 @@ package org.usfirst.frc.team3507.robot;
 public class RobotUtil {
 	
 	public static double deadzone(double side, double dead) {
+		double square;
 		if (side > dead || side < -dead) {
-    		return ((side-(Math.abs(side)/side*dead))/(1-dead));
+    		square = ((side-(Math.abs(side)/side*dead))/(1-dead));
+    		if (square < 0) {
+    			return -square * square;
+    		} else {
+    			return square * square;
+    		}
     	} else {
     		return 0.0;
     	}
